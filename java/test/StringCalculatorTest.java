a4dd170aef4c8425633f7e9546b4803b2234a8e8
Zan Markan <zan.markan@gmail.com>
2014-04-16 17:26:44 +0100

    first commit of java project

---
+++ java/test/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by zan on 16/04/2014.
+ */
+public class StringCalculatorTest {
+
+    @Test
+    public void an_empty_string_returns_0(){
+        Assert.assertTrue(false);
+    }
+}
