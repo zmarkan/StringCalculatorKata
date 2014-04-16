601ecdf87dd1c78322f01aae2e1d6bf88f51ef46
Chris Patuzzo <chris@patuzzo.co.uk>
2014-04-16 18:13:30 +0100

    Add a ruby template project

---
+++ ruby/lib/string_calculator.rb
@@ -0,0 +1,3 @@
+module StringCalculator
+
+end
---
+++ ruby/spec/string_calculator_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+require "string_calculator"
+
+describe StringCalculator do
+
+  it "returns 0 when given an empty string" do
+    true.should == false
+  end
+
+end
