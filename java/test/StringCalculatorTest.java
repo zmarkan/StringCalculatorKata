1ff116c60728dd86efe9986c6ad90e6af73e3473
Zan Markan <zan.markan@gmail.com>
2014-04-30 07:30:01 +0100

    fixed eclipse project

--- java/test/StringCalculatorTest.java
+++ java/test/StringCalculatorTest.java
@@ -8,6 +8,10 @@ public class StringCalculatorTest {
 
     @Test
     public void an_empty_string_returns_0(){
-        Assert.assertTrue(false);
+    	
+    	Assert.assertFalse(true);
     }
+       
 }
+
+
